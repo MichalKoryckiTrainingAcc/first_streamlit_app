107a75898f768a1449717f506bd809ac215101cf
MichalKoryckiTrainingAcc <114316428+MichalKoryckiTrainingAcc@users.noreply.github.com>
2022-09-25 00:24:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('DUM DUM DUM MODMDOMDODMODMOD')
+streamlit.header('Breakfast')
+streamlit.text('DSDSDDS')
+streamlit.text("sidjsijsdj")
+streamlit.text("AEIOU")
+streamlit.text("JOHN MADDEN")
