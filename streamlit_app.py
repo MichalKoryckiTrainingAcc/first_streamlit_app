b60582f48581a2faea22276c59a6c6aeef339226
MichalKoryckiTrainingAcc <114316428+MichalKoryckiTrainingAcc@users.noreply.github.com>
2022-09-25 21:03:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import snowflake.connector
+import pandas 
+import requests 
+from urllib.error import URLError
+
 streamlit.title('🍞DUM DUM DUM MODMDOMDODMODMOD')
 streamlit.header('Breakfast')
 streamlit.text('🥣   DSDSDDS')
@@ -7,7 +11,7 @@ streamlit.text("🥗sidjsijsdj")
 streamlit.text("🐔AEIOU")
 streamlit.text("🥑JOHN MADDEN")
 
-import pandas 
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
@@ -16,7 +20,7 @@ streamlit.dataframe(fruits_to_show)
 
 ## NEW SECTION
 streamlit.header("Fruityvice Fruit Advice!")
-import requests 
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 ### TAKE THE JSON and normalize
@@ -26,8 +30,7 @@ streamlit.dataframe(fruityvice_response)
 
 fruit_choice = streamlit.text_input("What fruit would you like information about?", "Kiwi")
 streamlit.write("The user entered ", fruit_choice)
-
-import requests 
+ 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruityvice_response = pandas.json_normalize(fruityvice_response.json())
 ### output as table
